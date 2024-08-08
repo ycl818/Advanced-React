8b38050b7c9accb37a08b87270ac965bbe4a64c8
yichien <kevinyichienlee@gmail.com>
2024-08-08 14:20:30 +0800

    screen splitter Enhancement

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { SplitScreen } from "./components/split-screen";
 
-const LeftSideComp = () => {
-  return <h2 style={{ backgroundColor: "crimson" }}>I am left!</h2>;
+const LeftSideComp = ({ title }) => {
+  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
 };
 
-const RightSideComp = () => {
-  return <h2 style={{ backgroundColor: "burlywood" }}>I am right!</h2>;
+const RightSideComp = ({ title }) => {
+  return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
 };
 
 function App() {
-  return <SplitScreen Left={LeftSideComp} Right={RightSideComp} />;
+  return (
+    <SplitScreen leftWidth={1} rightWidth={3}>
+      <LeftSideComp title={"Left"} />
+      <RightSideComp title={"Right"} />
+    </SplitScreen>
+  );
 }
 
 export default App;
--- src/components/split-screen.jsx
+++ src/components/split-screen.jsx
@@ -6,19 +6,16 @@ const Container = styled.div`
 `;
 
 const Panel = styled.div`
-  flex: 1;
+  flex: ${(props) => props.flex};
 `;
 
-export const SplitScreen = ({ Left, Right }) => {
+export const SplitScreen = ({ children, leftWidth = 1, rightWidth = 1 }) => {
+  const [left, right] = children;
   return (
     <Container>
-      <Panel>
-        <Left />
-      </Panel>
+      <Panel flex={leftWidth}>{left}</Panel>
 
-      <Panel>
-        <Right />
-      </Panel>
+      <Panel flex={rightWidth}>{right}</Panel>
     </Container>
   );
 };
