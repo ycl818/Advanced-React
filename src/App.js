9baeda55efcc81d495c56c9eeb7f73b41cdf6ae9
yichien <kevinyichienlee@gmail.com>
2024-08-08 13:41:57 +0800

    DP:Layout components - screen splitter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./components/split-screen";
+
+const LeftSideComp = () => {
+  return <h2 style={{ backgroundColor: "crimson" }}>I am left!</h2>;
+};
+
+const RightSideComp = () => {
+  return <h2 style={{ backgroundColor: "burlywood" }}>I am right!</h2>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SplitScreen Left={LeftSideComp} Right={RightSideComp} />;
 }
 
 export default App;
---
+++ src/components/split-screen.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Panel = styled.div`
+  flex: 1;
+`;
+
+export const SplitScreen = ({ Left, Right }) => {
+  return (
+    <Container>
+      <Panel>
+        <Left />
+      </Panel>
+
+      <Panel>
+        <Right />
+      </Panel>
+    </Container>
+  );
+};
